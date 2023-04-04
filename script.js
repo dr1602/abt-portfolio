176422838e757bc641bc82f2c57eaaa88bd81733
dr1602 <109833006+dr1602@users.noreply.github.com>
2023-04-04 17:55:03 -0600

    V3
    Update version of ABT landing page with Html, Css, & Javascript, with animated images, initial value proposition, and animated hero section.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
+
+menu.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active')
+}
+
+/*
 https://www.youtube.com/watch?v=eQH7WbM3QIg&list=PLd9t-kSer9IKoDYe6np_pegcrMkfX9N5U&index=108
+*/
